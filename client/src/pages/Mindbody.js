c88f4082a8df880b2fde9a4e94c907b9ce677dfa
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-08-13 22:27:20 -0700

    added grid and paper components from material ui, added Facebook code, linking but not appearing, needs de-bugging

--- client/src/pages/Mindbody.js
+++ client/src/pages/Mindbody.js
@@ -1,21 +1,69 @@
-import React from 'react';
-import ThoughtList from '../components/ThoughtList';
-import ThoughtForm from '../components/ThoughtForm';
-import FriendList from '../components/FriendList';
+/** @format */
 
-import Auth from '../utils/auth';
-import { useQuery } from '@apollo/react-hooks';
-import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
+import React from "react";
+import Auth from "../utils/auth";
+import { useQuery } from "@apollo/react-hooks";
+import { QUERY_ME_BASIC } from "../utils/queries";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import { FacebookProvider, Page } from 'react-facebook';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: "center",
+        color: theme.palette.text.secondary,
+    },
+}));
+
 const Mindbody = () => {
-    const { loading, data } = useQuery(QUERY_THOUGHTS);
+    const classes = useStyles();
     const { data: userData } = useQuery(QUERY_ME_BASIC);
-    const thoughts = data?.thoughts || [];
     const loggedIn = Auth.loggedIn();
     console.log(userData);
     return (
         <main>
-            <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
-            <healcode-widget data-type="schedules" data-widget-partner="object" data-widget-id="ea1646006627" data-widget-version="1" ></healcode-widget>
+            <div className={classes.root}>
+                <Grid
+                    container
+                    spacing={3}
+                    direction="row"
+                    justifyContent="space-around"
+                    alignItems="space-evenly"
+                >
+                    <Grid item xs={6}>
+                        <Paper className={classes.paper}>
+                            Class Schedule
+                            <healcode-widget
+                                data-type="schedules"
+                                data-widget-partner="object"
+                                data-widget-id="ea1646006627"
+                                data-widget-version="1"
+                            ></healcode-widget>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Paper className={classes.paper}>Facebook
+                            <FacebookProvider appId="113518392061132">
+                                <Page href="https://www.facebook.com/PhoenixCenterForTheArts/" tabs="timeline" />
+                            </FacebookProvider>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Paper className={classes.paper}>Twitter</Paper>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Paper className={classes.paper}>Linked In</Paper>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Paper className={classes.paper}>TicTok</Paper>
+                    </Grid>
+                </Grid>
             </div>
         </main>
     );

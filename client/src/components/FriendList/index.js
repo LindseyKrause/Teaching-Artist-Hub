c8217bc0e8569bfd898053ed7b5eacadfcafbc0f
Kellie Werrell <kelliewerrell@gmail.com>
2021-08-13 21:33:46 -0700

    trying color schemes

--- client/src/components/FriendList/index.js
+++ client/src/components/FriendList/index.js
@@ -12,7 +12,7 @@ const FriendList = ({ friendCount, username, friends }) => {
         {username}'s {friendCount} {friendCount === 1 ? 'friend' : 'friends'}
       </h5>
       {friends.map(friend => (
-        <button className="btn w-100 display-block mb-2" key={friend._id}>
+        <button className="btn2 w-100 display-block mb-2" key={friend._id}>
           <Link to={`/profile/${friend.username}`}>{friend.username}</Link>
         </button>
       ))}

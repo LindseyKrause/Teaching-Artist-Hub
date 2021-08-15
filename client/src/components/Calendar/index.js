6b4b7449f3da79d4889dfdb7212628c15a6529fb
Joe Porrazzo Jr <joeporrazzojr@joes-mac-mini.lan>
2021-08-15 16:07:22 -0700

    moved sensitive calendar info to env file

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Calendar from '@ericz1803/react-google-calendar';
 
-const API_KEY = 'AIzaSyCTLwpyb-j-woXYEA2vF04Y2cBPLBydvto';
+const calendarAPI = process.env.API_KEY;
 
 let calendars = [
-    { calendarId: 'c_328os62b0238c6h93arsgpj4jo@group.calendar.google.com' }
+    { calendarId: process.env.CALENDAR_ID }
 ];
 
 function GoogleCalendar () {
     return (
         <section>
-            <Calendar apiKey={API_KEY} calendars={calendars} />
+            <Calendar apiKey={calendarAPI} calendars={calendars} />
         </section>
     );
 }

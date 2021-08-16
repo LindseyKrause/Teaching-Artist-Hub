da852f61000846aed6dfbfd45e9fd92372508c29
VeeMcGee <vmcguire6377@outlook.com>
2021-08-15 22:23:42 -0600

    improved forms button styling

--- client/src/pages/Forms.js
+++ client/src/pages/Forms.js
@@ -5,28 +5,32 @@ import forms_1 from '../../src/images/forms_1.png';
 
 
 const Forms = () => (
-<form id="forms" className="btn">
+<form id="forms" className="links">
             
             <h3>Please click the icons below to access forms.</h3>
+            <br></br>
   <a href="https://drive.google.com/drive/folders/1mb3Lq3XdxE8r-Rkys3MtyeKOSxZ-R5Tm?usp=sharing">
-  <button className="btn col-12 col-md-3" type="submit"></button>
+  <div className="form" type="submit"></div>
   📝   Download Administrator Forms 
         </a>
 <br></br>
+<br></br>
+
   <a href="https://docs.google.com/forms/d/e/1FAIpQLSfVIjyg-9rd_VmKDW3cGzsYBxYZZgq4GGLVSAkAm-m2bMQFNw/viewform?usp=sf_link">
-  <button className="btn col-12 col-md-3" type="submit"></button>
+  <div className="form" type="submit"></div>
   📝 Upload Administrator Forms 
         </a>
         <br></br>
+        <br></br>
   <a href="https://drive.google.com/drive/folders/1-dSjyb0Ocr50-8aAVhCJ1Ft_SDBuap8s?usp=sharing">
-  <button className="btn col-12 col-md-3" type="submit"></button>
+  <div className="form" type="submit"></div>
   📝 Class Calls and Proposals 
         </a>
         <br></br>
-  
+        <br></br>
   <a href="mailto:centralartsalliance@bill.com, krause@phoenixcenterforthearts.org,
 j.valiente@phoenixcenterforthearts.org">
-  <button className="btn col-12 col-md-3" type="submit"></button>
+  <div className="form" type="submit"></div>
   📧 Submit Finance Forms
         </a>
         <br></br>

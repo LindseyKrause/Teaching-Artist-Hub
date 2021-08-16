ba9774e95d7967bacdda24aac87c27aa341836ac
VeeMcGee <vmcguire6377@outlook.com>
2021-08-15 21:15:32 -0600

    added buttons and related links for forms, downloads, uploads

--- client/src/pages/Forms.js
+++ client/src/pages/Forms.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import Download from '../../src/components/DLFOLDER';
+
+import forms_1 from '../../src/images/forms_1.png';
 
 
-const Forms = () => {
-    return (
-        <div>
-            <Download>
-            </Download>
-        </div>
-    );
-};
 
-export default Download;
+const Forms = () => (
+<section id="forms" className="forms">
+            
+            <h3>Please click the icon below to download Administrator Forms.</h3>
+  <a href="https://drive.google.com/drive/folders/1mb3Lq3XdxE8r-Rkys3MtyeKOSxZ-R5Tm?usp=sharing">
+  <img src={forms_1} style= {{width: "15%", height: "8%" }} alt="forms icon"></img>
+  </a>
+
+  <h3>Please click the icon below to upload Administrator Forms.</h3>
+  <a href="https://docs.google.com/forms/d/e/1FAIpQLSfVIjyg-9rd_VmKDW3cGzsYBxYZZgq4GGLVSAkAm-m2bMQFNw/viewform?usp=sf_link">
+  <img src={forms_1} style= {{width: "15%", height: "8%" }} alt="forms icon"></img>
+  </a>
+
+  <h3>Please click the icon below to view current Class Calls.</h3>
+  <a href="https://drive.google.com/drive/folders/1-dSjyb0Ocr50-8aAVhCJ1Ft_SDBuap8s?usp=sharing">
+      <img src={forms_1} style= {{width: "15%", height: "8%" }} alt="forms icon"></img>
+  </a>
+
+  <h3>Please click here to submit finance forms.</h3>
+  <a href="https://drive.google.com/drive/folders/1-dSjyb0Ocr50-8aAVhCJ1Ft_SDBuap8s?usp=sharing">
+      <img src={forms_1} style= {{width: "15%", height: "8%" }} alt="forms icon"></img>
+  </a>
+
+
+</section>
+);
+
+
+
+export default Forms;

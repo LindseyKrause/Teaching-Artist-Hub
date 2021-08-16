1e26459ed1fb5b13617b38d591f3fd7c2a100d67
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-08-15 22:09:03 -0700

    added logos to image file and footer information with logo.  Still needs formatter/styled

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer id="headerbg" className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2021</div>
+    <footer className="footer">
+      <div className="container">
+        <div className="container">
+          <a href="https://phoenixcenterforthearts.org/" target="_blank">
+            <img src="https://i.imgur.com/hkNU3mP.png" alt="Logo"></img></a>
+        <h6>&copy;2021Central Arts Alliance 1202 North 3rd Street, Phoenix, AZ 85004 (602) 254-3100
+      </h6>
+            </div>
+
+      </div>
     </footer>
   );
 };

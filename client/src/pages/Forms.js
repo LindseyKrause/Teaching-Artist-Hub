215a9a9bdd81172cc2522cd6cd4b60a239db5bf8
VeeMcGee <vmcguire6377@outlook.com>
2021-08-15 22:11:43 -0600

    added button styling to forms links, added emails for finance forms

--- client/src/pages/Forms.js
+++ client/src/pages/Forms.js
@@ -5,31 +5,36 @@ import forms_1 from '../../src/images/forms_1.png';
 
 
 const Forms = () => (
-<section id="forms" className="forms">
+<form id="forms" className="btn">
             
-            <h3>Please click the icon below to download Administrator Forms.</h3>
+            <h3>Please click the icons below to access forms.</h3>
   <a href="https://drive.google.com/drive/folders/1mb3Lq3XdxE8r-Rkys3MtyeKOSxZ-R5Tm?usp=sharing">
-  <img src={forms_1} style= {{width: "15%", height: "8%" }} alt="forms icon"></img>
-  </a>
-
-  <h3>Please click the icon below to upload Administrator Forms.</h3>
+  <button className="btn col-12 col-md-3" type="submit"></button>
+  📝   Download Administrator Forms 
+        </a>
+<br></br>
   <a href="https://docs.google.com/forms/d/e/1FAIpQLSfVIjyg-9rd_VmKDW3cGzsYBxYZZgq4GGLVSAkAm-m2bMQFNw/viewform?usp=sf_link">
-  <img src={forms_1} style= {{width: "15%", height: "8%" }} alt="forms icon"></img>
-  </a>
-
-  <h3>Please click the icon below to view current Class Calls.</h3>
+  <button className="btn col-12 col-md-3" type="submit"></button>
+  📝 Upload Administrator Forms 
+        </a>
+        <br></br>
   <a href="https://drive.google.com/drive/folders/1-dSjyb0Ocr50-8aAVhCJ1Ft_SDBuap8s?usp=sharing">
-      <img src={forms_1} style= {{width: "15%", height: "8%" }} alt="forms icon"></img>
-  </a>
-
-  <h3>Please click here to submit finance forms.</h3>
-  <a href="https://drive.google.com/drive/folders/1-dSjyb0Ocr50-8aAVhCJ1Ft_SDBuap8s?usp=sharing">
-      <img src={forms_1} style= {{width: "15%", height: "8%" }} alt="forms icon"></img>
-  </a>
-
-
-</section>
+  <button className="btn col-12 col-md-3" type="submit"></button>
+  📝 Class Calls and Proposals 
+        </a>
+        <br></br>
+  
+  <a href="mailto:centralartsalliance@bill.com, krause@phoenixcenterforthearts.org,
+j.valiente@phoenixcenterforthearts.org">
+  <button className="btn col-12 col-md-3" type="submit"></button>
+  📧 Submit Finance Forms
+        </a>
+        <br></br>
+</form>
 );
+
+
+;
 
 
 

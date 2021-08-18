d6d107d35852c58e0721aac057bc4434e9935b7c
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-08-18 16:01:59 -0700

    added title to iframe

--- client/src/components/Twitter/index.js
+++ client/src/components/Twitter/index.js
@@ -5,7 +5,7 @@ import React from "react";
 const TwitterInfo = () => {
 	return (
 		<div>
-			<iframe className="twitter-timeline"data-theme="dark"href="https://twitter.com/PhxCenterArts?ref_src=twsrc%5Etfw"data-tweet-limit="3">Tweets by PhxCenterArts   </iframe>
+			<iframe title="Twitter Feed" className="twitter-timeline" data-theme="dark" src="https://twitter.com/PhxCenterArts?ref_src=twsrc%5Etfw" data-tweet-limit="3" >Tweets by PhxCenterArts</iframe>
 		</div>
 	);
 };

14971e4b8fb53e46a6fa21d5a3b8e3cc22e81d4f
Lindsey Krause <scarrlettearianne@gmail.com>
2022-03-03 18:44:29 -0700

    Removed Calendar

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Calendar from '@ericz1803/react-google-calendar';
 
-const calendarAPI = 'AIzaSyCTLwpyb-j-woXYEA2vF04Y2cBPLBydvto';
+
 
 let calendars = [
     { calendarId: 'c_328os62b0238c6h93arsgpj4jo@group.calendar.google.com' }
